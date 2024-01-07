d4e7dcbeb81dba3dc2b47813b41adc25f16cf587
Michael <mickyonyango1544@gmail.com>
2024-01-07 19:20:42 +0300

    My to do App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,42 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TodoItem } from './todoItem';
+import { TodoList } from './todoList';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms'
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatTableModule } from '@angular/material/table';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,MatSlideToggleModule,MatInputModule,MatFormFieldModule,MatCheckboxModule,MatTableModule,MatBadgeModule,MatIconModule,MatToolbarModule,MatButtonModule,FormsModule,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Umber';
+  private list=new TodoList("Mike",[new TodoItem("Go for a run",true),new TodoItem("Get Some flowers"),new TodoItem("Play Some Reggae Music")]);
+  get username():string{
+    return this.list.user
+  }
+  get itemCount():number{
+    return this.list.items.filter(item=>!item.complete).length;
+  }
+  get items():readonly TodoItem[]{
+    return this.list.items.filter(items=>this.showComplete||!items.complete)
+  }
+  addItem(newItem:string){
+    if(newItem!=""){
+      this.list.addItem(newItem)
+    }
+  }
+  showComplete:boolean=false;
 }
---
+++ src/app/todoList.ts
@@ -0,0 +1,11 @@
+import { TodoItem } from "./todoItem";
+
+export class TodoList{
+  constructor(public user:string,private todoItems:TodoItem[]){}
+  get items(): readonly TodoItem[]{
+    return this.todoItems
+  }
+  addItem(task:string){
+    this.todoItems.push(new TodoItem(task));
+  }
+}
